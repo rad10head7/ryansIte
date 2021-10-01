ae2b1dc0f7bb19f2636edc43dd34eb9afc094de9
corey <52474495+rad10head7@users.noreply.github.com>
2021-09-30 23:58:46 -0400

    small changes

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -2,6 +2,4 @@ export * from './buttons';
 export * from './containerLayout';
 export * from './mainContent';
 export * from './sectionIntro';
-export * from './tags';
-export * from './underLink';
 export * from './global';
